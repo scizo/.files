2a0f4d5a309d269b640ce4a6e758e155d2447b53
Scott Nielsen <scottnielsen5@gmail.com>
2026-08-22 00:55:54 -0600

    Missed setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ fi
 dir=$(cd "$(dirname "$BASH_SOURCE")"; pwd)
 
 if [ $# -eq 0 ]; then
-  files=$(ls $dir | grep -v 'setup.sh' | grep -v '.gitignore' | grep -v '.gitmodules' | grep -v 'init.vim' | grep -v 'clojure' | grep -v 'ghostty')
+  files=$(ls $dir | grep -v 'setup.sh' | grep -v '.gitignore' | grep -v '.gitmodules' | grep -v 'init.vim' | grep -v 'clojure' | grep -v 'ghostty' | grep -v 'tmux')
 else
   files=$@
 fi
@@ -39,4 +39,11 @@ if [ -e "$HOME/.config/ghostty" ]; then
 else
   echo "linking ghostty"
   ln -s $dir/ghostty $HOME/.config/ghostty
+fi
+
+if [ -e "$HOME/.config/tmux" ]; then
+  echo ".config/tmux exists ... skipping"
+else
+  echo "linking tmux"
+  ln -s $dir/tmux $HOME/.config/tmux
 fi
